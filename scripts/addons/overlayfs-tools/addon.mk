3db06b878d6e944f481822595ebc1cb24020830f
scpcom <scpcom@gmx.de>
2026-06-18 02:07:20 +0200

    addons: overlayfs-tools: define and use OVERLAYFS_TOOLS_PACKAGE_DIR

--- scripts/addons/overlayfs-tools/addon.mk
+++ scripts/addons/overlayfs-tools/addon.mk
@@ -5,17 +5,19 @@ endif
 
 OVERLAYFS_TOOLS_VERSION = 2025.01
 
+OVERLAYFS_TOOLS_PACKAGE_DIR = $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)
+
 $(BUILDDIR)/overlayfs-tools-stamp: $(BUILDDIR)/buildroot-package-stamp
 	@echo "$(COLOUR_GREEN)Packaging overlayfs-tools for $(BOARD)$(END_COLOUR)"
 	@$(eval BV=$(shell cd $(BUILDDIR)/buildroot && git log -1 --format="%at" | xargs -I{} date -d @{} +-%Y%m%d-${KERNELREV}))
-	@mkdir -p $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)
-	@cp -r /builder/deb/overlayfs-tools/* $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/
-	@mkdir -pv $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/usr/bin/
-	@cp -p $(BR_OUTPUT_DIR)/target/usr/bin/fsck.overlay $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/usr/bin/
-	@cp -p $(BR_OUTPUT_DIR)/target/usr/bin/overlay $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/usr/bin/
-	@sed -i 's/Architecture: riscv64/Architecture: $(DEB_ARCH)/' $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/DEBIAN/control
-	@sed -i 's/Version: 1.0.0-1/Version: $(OVERLAYFS_TOOLS_VERSION)$(BV)/' $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/DEBIAN/control
-	@sed -i 's/Package: overlayfs-tools/Package: overlayfs-tools/' $(BUILDDIR)/package/overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION)/DEBIAN/control
+	@mkdir -p $(OVERLAYFS_TOOLS_PACKAGE_DIR)
+	@cp -r /builder/deb/overlayfs-tools/* $(OVERLAYFS_TOOLS_PACKAGE_DIR)/
+	@mkdir -pv $(OVERLAYFS_TOOLS_PACKAGE_DIR)/usr/bin/
+	@cp -p $(BR_OUTPUT_DIR)/target/usr/bin/fsck.overlay $(OVERLAYFS_TOOLS_PACKAGE_DIR)/usr/bin/
+	@cp -p $(BR_OUTPUT_DIR)/target/usr/bin/overlay $(OVERLAYFS_TOOLS_PACKAGE_DIR)/usr/bin/
+	@sed -i 's/Architecture: riscv64/Architecture: $(DEB_ARCH)/' $(OVERLAYFS_TOOLS_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/Version: 1.0.0-1/Version: $(OVERLAYFS_TOOLS_VERSION)$(BV)/' $(OVERLAYFS_TOOLS_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/Package: overlayfs-tools/Package: overlayfs-tools/' $(OVERLAYFS_TOOLS_PACKAGE_DIR)/DEBIAN/control
 	@cd $(BUILDDIR)/package/ && dpkg-deb --build overlayfs-tools-$(OVERLAYFS_TOOLS_VERSION) overlayfs-tools_$(OVERLAYFS_TOOLS_VERSION)$(BV)_$(DEB_ARCH).deb
 	@cp $(BUILDDIR)/package/overlayfs-tools_$(OVERLAYFS_TOOLS_VERSION)$(BV)_$(DEB_ARCH).deb /output/
 	@mkdir -p /rootfs/tmp/install/
